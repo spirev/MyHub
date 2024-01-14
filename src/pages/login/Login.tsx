4aeea42234a4a4b288efd2750f0ff478ac697030
spirev <prospirev@gmail.com>
2024-01-14 23:56:23 +0100

    Build login page for mobile with new components

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,16 @@
+import Button from '../../components/button/Button';
+import VerticalContainer from '../../components/verticalContainer/VerticalContainer';
 import './login.css';
 
 function Login() {
   return (
-    <div id="page-container">
+    <VerticalContainer className="login-page-container">
       <h1>Login</h1>
-    </div>
+      <VerticalContainer className="action-container">
+        <a className="demo-link">Try the demo profil</a>
+        <Button>Send authentification code</Button>
+      </VerticalContainer>
+    </VerticalContainer>
   );
 }
 
