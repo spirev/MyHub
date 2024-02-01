a23504948e657877acf357d4990362c336a9c45e
spirev <59651873+spirev@users.noreply.github.com>
2024-02-01 23:28:48 +0100

    Add onClick property to Button component. (#10)
    * Add onClick property to Button component.
    ( + minor css change )
    
    * Allow default html props to work on Button component
    
    * temporary push for work
    
    * Refacto login page component into two sub-component

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,20 +1,29 @@
-import Button from '../../components/button/Button';
 import LoginGroundWaves from './loginGroundWaves/LoginGroundWaves';
 import VerticalContainer from '../../components/verticalContainer/VerticalContainer';
 import './login.css';
 import LoginBubbles from './loginBubbles/LoginBubbles';
 import "./loginBubbles/LoginBubbles.css";
+import { useState } from 'react';
+import AccountSelection from './accountSelection/AccountSelection';
+import AuthCode from './authCode/AuthCode';
 
 function Login() {
+  const [isCodeSent, setIsCodeSent] = useState(false);
+  const [authCode, setAuthCode] = useState('');
+
   return (
     <VerticalContainer className="login-page-container">
       <VerticalContainer className="desktop-background-container">
         <VerticalContainer className="mobile-content-container">
           <h1>Login</h1>
-          <VerticalContainer className="action-container">
-            <a className="demo-link">Try the demo profile</a>
-            <Button>Get code</Button>
-          </VerticalContainer>
+          {isCodeSent ? (
+            <AuthCode
+              authCode={authCode}
+              setAuthCode={setAuthCode}
+            />
+          ) : (
+            <AccountSelection setIsCodeSent={setIsCodeSent} />
+          )}
         </VerticalContainer>
       </VerticalContainer>
       <LoginGroundWaves />
